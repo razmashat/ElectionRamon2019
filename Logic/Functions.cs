50cc9fdce0c2cf6b68bcd1d6a0c0daf8a06a73bb
User <User@LAB3-04>
2019-09-09 14:35:57 +0300

    some shit

---
+++ Logic/Functions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    class Functions
+    {
+
+
+
+        public bool login(int id) {
+
+            return true;
+
+        }
+
+
+
+
+    }
+}
